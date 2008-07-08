c896b2b30e43df6f655f5391e1d681063beff037
hortelanobruno <hortelanobruno@d20db40b-6351-0410-bc54-079dc1644049>
2008-07-08 15:34:08 +0000

    Initial import.

---
+++ trunk/JSP/src/src/Bruno.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Bruno {
+
+	private String nombre;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Bruno() {
+		this.setNombre("Bruno");
+	}
+
+}
